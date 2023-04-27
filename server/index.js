d6861cc224cb7ce5217d1dad98993f9098a44b6b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-27 08:15:00 -0400

    set up routes directory and created login route

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
 import {register} from './controllers/auth.js'
 
 const __filename = fileURLToPath(import.meta.url);
@@ -36,6 +37,9 @@ const upload =  multer({storage});
 
 //Routes With Files
 app.post('/auth/register', upload.single('picture'), register);
+
+//Routes not dependent on files/multer
+app.use('/auth', authRoutes);
 
 
 //Mongoose setup
