c40f36d0c563d97c5c844db42cba09565a8de5e4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-04-29 09:54:40 -0400

    set up app.use to accept as yet created posts routes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 import {register} from './controllers/auth.js'
+import {createPost} from './controllers/posts.js'
+import { verifyToken } from './middleware/auth.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -38,10 +41,12 @@ const upload =  multer({storage});
 
 //Routes With Files
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
 //Routes not dependent on files/multer
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 //Mongoose setup
 const PORT = process.env.PORT || 6001;
