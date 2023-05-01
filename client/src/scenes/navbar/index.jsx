8ad403ca50a8935bcaf00865c124c6ba09202f53
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-01 15:55:25 -0400

    created a host of files and folders for components, state, and scene

---
+++ client/src/scenes/navbar/index.jsx
@@ -0,0 +1,8 @@
+
+
+const Navbar = () => {
+    return (
+      <div>lNavbar</div>
+    )
+  }
+  export default Navbar;
---
+++ client/src/scenes/profilePage/index.jsx
@@ -0,0 +1,8 @@
+
+
+const ProfilePage = () => {
+    return (
+      <div>profile page</div>
+    )
+  }
+  export default ProfilePage;
