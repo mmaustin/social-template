b13f2d43fff375b7705f4fc3104ea90ade825ad4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-01 08:58:40 -0400

    inserted mock data into mongodb

--- server/models/Post.js
+++ server/models/Post.js
@@ -31,4 +31,5 @@ const PostSchema = mongoose.Schema(
     {timestamps: true}
 );
 
-export default mongoose.model("Post", PostSchema);
+const Post = mongoose.model("Post", PostSchema);
+export default Post;
