082dccf77d02e413d58b7dce753a32b7dd8ccf2a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-02 08:04:47 -0400

    set up routes with react router dom components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+
 
 function App() {
+
+
+
   return (
     <div className="app">
-      baron von munchhausen
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage/>} />
+          <Route path='/home' element={<HomePage/>} />
+          <Route path='/profile/:id' element={<ProfilePage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
