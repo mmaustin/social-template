00f4e0599796f875311302896d2db811b42b0284
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-03 10:36:48 -0400

    more navbar menu and select stylings

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -49,7 +49,48 @@ const Navbar = () => {
           </FlexBetween>
         )}
       </FlexBetween>
+      {/* desktop nav */}
+      {isNonMobileScreens ? (
+        <FlexBetween gap="2rem">
+          <IconButton onClick={()=> dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkMode sx={{fontSize: "25px"}}/>
+            ) : (
+              <LightMode sx={{color: dark, fontSize: "25px"}} />
+            )}
+          </IconButton>
+          <Message sx={{color: dark, fontSize: "25px"}} />
+          <Notifications sx={{color: dark, fontSize: "25px"}} />
+          <Help sx={{color: dark, fontSize: "25px"}} />
+          <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1 rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem"
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight
+                  }
+                }}
+                input={<InputBase/>}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem onClick={()=> dispatch(setLogout())}>Log Out</MenuItem>
+              </Select>
+          </FormControl>
+        </FlexBetween>
+        ) : (
+        <IconButton></IconButton>
+        )}
     </FlexBetween>
-  )
-}
+  );
+};
 export default Navbar;
