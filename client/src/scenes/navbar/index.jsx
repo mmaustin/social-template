806f522624c012d83c0218893436f63e657053e8
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-03 12:29:35 -0400

    aimported navbar component to homepage component

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -114,6 +114,43 @@ const Navbar = () => {
                 <Close/>
               </IconButton>
             </Box>
+          {/* Menu Items */}
+          <FlexBetween display="flex" flexDirection="column" justifyContent="center" alignItems="center" gap="2rem">
+            <IconButton onClick={()=> dispatch(setMode())} sx={{fontSize: "25px"}}>
+              {theme.palette.mode === "dark" ? (
+                <DarkMode sx={{fontSize: "25px"}}/>
+              ) : (
+                <LightMode sx={{color: dark, fontSize: "25px"}} />
+              )}
+            </IconButton>
+            <Message sx={{color: dark, fontSize: "25px"}} />
+            <Notifications sx={{color: dark, fontSize: "25px"}} />
+            <Help sx={{color: dark, fontSize: "25px"}} />
+            <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1 rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem"
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight
+                  }
+                }}
+                input={<InputBase/>}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem onClick={()=> dispatch(setLogout())}>Log Out</MenuItem>
+              </Select>
+            </FormControl>
+          </FlexBetween>          
           </Box>
         )}
     </FlexBetween>
