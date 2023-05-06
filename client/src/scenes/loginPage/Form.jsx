9542c2bd34648a39f133b4f0e97d63dca75f195a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-06 11:32:58 -0400

    added email and password fields to form component

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -152,6 +152,7 @@ const Form = () => {
                                             ) : (
                                                 <FlexBetween>
                                                     <Typography>{values.picture.name}</Typography>
+                                                    <EditOutLinedIcon/>
                                                 </FlexBetween>
                                             )}
                                         </Box>
@@ -160,7 +161,29 @@ const Form = () => {
                             </Box>
                         </>
                     )}
+                    <TextField
+                        label="Email"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.email}
+                        name="email"
+                        error={Boolean(touched.email) && Boolean(errors.email)}
+                        helperText={touched.email && errors.email}
+                        sx={{gridColumn: "span 4"}}
+                    />                    
+                    <TextField
+                        label="Password"
+                        type="password"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.password}
+                        name="password"
+                        error={Boolean(touched.password) && Boolean(errors.password)}
+                        helperText={touched.password && errors.password}
+                        sx={{gridColumn: "span 4"}}
+                    />                    
                 </Box>
+                
             </form>
         )}
     </Formik>
