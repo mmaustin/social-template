27b40603519ab49f7ae0586c4470cd26f9510e79
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-07 11:24:44 -0400

    created the first and second rows of our the user widget component

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -21,5 +21,70 @@ const UserWidget = ({userId, picturePath}) => {
   const dark = palette.neutral.dark;
   const medium = palette.neutral.medium;
   const main = palette.neutral.main;
-  
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:5001/users/${userId}`, {
+      method: 'GET',
+      headers: {Authorization: `Bearer ${token}`},
+    });
+    const data = response.json();
+    setUser(data);
+  };
+
+  useEffect(()=>{
+    getUser();
+  }, []) //eslint-disable-line react-hooks/exhaustive-deps
+
+  if(!user){
+    return null;
+  }
+
+  const {firstName, lastName, location, occupation, viewedProfile, impressions, friends} = user;
+
+  return (
+    <WidgetWrapper>
+      {/* first row */}
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
+        <FlexBetween gap="1rem">
+          <UserImage image={picturePath}/>
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              sx={{
+                "&:hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer"
+                }
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}>{friends.length} friends</Typography>
+          </Box>
+          <ManageAccountsOutlined/>
+        </FlexBetween>
+      </FlexBetween>
+      <Divider/>
+
+      {/* second row */}
+      <Box p="1rem 0">
+        <Box display="flex" alignItems="center" gap="1rem" mb="0.5">
+          <LocationOnOutlined fontSize="large" sx={{color: main}}/>
+          <Typography color={medium}>{location}</Typography>
+        </Box>
+        <Box display="flex" alignItems="center" gap="1rem">
+          <LocationOnOutlined fontSize="large" sx={{color: main}}/>
+          <Typography color={medium}>{occupation}</Typography>
+        </Box>
+      </Box>
+
+    </WidgetWrapper>
+  )
+
 }
