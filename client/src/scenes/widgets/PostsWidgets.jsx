fb5d17f77f3a4105d5f8ca1d8eb7eec061fc41c6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-09 11:12:49 -0400

    set up api calls to posts routes and conditional use effect implementation

---
+++ client/src/scenes/widgets/PostsWidgets.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import PostWidget from "./PostWidget";
+
+const PostsWidgets = ({userId, isProfile}) => {
+
+  const dispatch = useDispatch();
+  const posts = useSelector(state => state.posts);
+  const token = useSelector(state => state.token);
+
+  const getPosts = async () => {
+    const response = await fetch('http://localhost:5001/posts', {
+      method: "GET",
+      headers: {Authorization: `Bearer ${token}`}
+    });
+    const data = await response.json();
+    dispatch(setPosts({posts: data}));
+  }
+
+  const getUserPosts = async () => {
+    const response = await fetch(`http://localhost:5001/posts/${userId}/posts`, {
+      method: "GET",
+      headers: {Authorization: `Bearer ${token}`}
+    });
+    const data = await response.json();
+    dispatch(setPosts({posts: data}));
+  }
+
+  useEffect(() => {
+    if(isProfile){
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []) //eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>PostsWidgets</div>
+  )
+}
+export default PostsWidgets
