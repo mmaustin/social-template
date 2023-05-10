e53961952d176dee173bfb2c00581dcfb7bdcc5e
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-10 08:54:39 -0400

    added initial mui flexbetween, typography, and box components to friend component

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -34,11 +34,39 @@ const Friend = ({friendId, name, subtitle, userPicturePath}) => {
         },
       }
     );
-    
+    const data = await response.json();
+    dispatch(setFriends({friends: data}))
   }
 
   return (
-    <div>Friend</div>
+    <FlexBetween>
+      <FlexBetween gap='1rem'>
+        <UserImage image={userPicturePath} size="55px"/>
+        <Box
+          onClick={() => {
+            navigate(`/profile/${friendId}`);
+            navigate(0);
+          }}
+        >
+          <Typography
+            color={main}
+            variant="h5"
+            fontWeight="500"
+            sx={{
+              "&:hover": {
+                color: palette.primary.light,
+                cursor: "pointer"
+              }
+            }}
+          >
+            {name}
+          </Typography>
+          <Typography color={medium} fontSize="0.75rem">
+            {subtitle}
+          </Typography>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
   )
 }
 export default Friend
