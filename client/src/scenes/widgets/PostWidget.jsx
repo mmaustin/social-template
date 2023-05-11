dea6e9c1f2c6565bd08a7be74565bc33a318dacc
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-05-11 08:11:14 -0400

    completed post widget

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -63,11 +63,12 @@ const PostWidget = ({
             height='auto'
             alt='post'
             style={{borderRadius: '0.75rem', marginTop: '0.75ren'}}
-            src={`http://localhost:5001/public/assets/${picturePath}`}
+            src={`http://localhost:5001/assets/${picturePath}`}
           />
         )}
         <FlexBetween mt='0.25rem'>
           <FlexBetween gap='1rem'>
+
             <FlexBetween gap='0.3rem'>
               <IconButton onClick={patchLike}>
                 {isLiked ? ( 
@@ -78,8 +79,32 @@ const PostWidget = ({
               </IconButton>
               <Typography>{likeCount}</Typography>
             </FlexBetween>
+
+            <FlexBetween gap='0.3rem'>
+              <IconButton onClick={() => setIsComments(!isComments)}>
+                <ChatBubbleOutlineOutlined/>
+              </IconButton>
+              <Typography>{comments.length}</Typography>
+            </FlexBetween>
           </FlexBetween>
+
+          <IconButton>
+                <ShareOutlined/>
+          </IconButton>
         </FlexBetween>
+        {isComments && (
+          <Box mt='0.5rem'>
+            {comments.map((comment, i) => (
+              <Box key={`${name}-${i}`}>
+                <Divider/>
+                <Typography sx={{color: main, m: '0.5rem 0', pl: '1rem'}}>
+                  {comment}
+                </Typography>
+              </Box>
+            ))}
+            <Divider/>
+          </Box>
+        )}
     </WidgetWrapper>
   )
 }
