4f51ed71d9736d693cfb7ae3b7c850ff11e829bc
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-06-23 11:19:44 -0400

    created pagebuttonwidget component

---
+++ client/src/scenes/widgets/PageButtonWidget.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@mui/material";
+import WidgetWrapper from "components/WidgetWrapper";
+
+
+
+const PageButtonWidget = () => {
+  return (
+    <WidgetWrapper>
+      <Button
+        sx={{color: 'blue'}}
+      >
+        Button
+      </Button>
+    </WidgetWrapper>
+  )
+}
+export default PageButtonWidget
